35309e0ca14b3a369df3f7ba5e493325612d36c4
Ethan Epperly <eepperly@caltech.edu>
2024-05-10 18:24:48 -0700

    Add sketched KW estimate

--- code/kw_estimate.m
+++ code/kw_estimate.m
@@ -5,7 +5,15 @@ function be = kw_estimate(A, b, y, varargin)
         theta = varargin{1};
     end
 
-    if length(varargin) >= 3
+    if length(varargin) >= 2 && strcmp(varargin{2}, "sketched")
+        if length(varargin) >= 3
+            d = varargin{3};
+        else
+            d = 2*size(A,2);
+        end
+        sketch = sparsesign(d,size(A,1),8);
+        [~,S,V] = svd(full(sketch*A),'econ');
+    elseif length(varargin) >= 3
         S = varargin{2};
         V = varargin{3};
     else
@@ -19,5 +27,5 @@ function be = kw_estimate(A, b, y, varargin)
         omega = theta*norm(r) / sqrt(1 + theta^2*norm(x)^2);
     end
 
-    be = omega / norm(r) * norm((V'*A'*r) ./ (diag(S).^2 + omega^2).^0.5);
+    be = omega / norm(r) * norm((V'*(A'*r)) ./ (diag(S).^2 + omega^2).^0.5);
 end
