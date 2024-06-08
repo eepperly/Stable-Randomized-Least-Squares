bd1430bcfb349ed7b4935a3c51c79bcd6c4f448d
Ethan Epperly <eepperly@caltech.edu>
2024-06-07 17:37:54 -0700

    Make random_ls_problem more efficient

--- utils/haarorth.m
+++ utils/haarorth.m
@@ -1,4 +1,9 @@
-function Q = haarorth(m)
-[Q,R] = qr(randn(m));
+function Q = haarorth(m,varargin)
+if ~isempty(varargin)
+    n = varargin{1};
+else
+    n = m;
+end
+[Q,R] = qr(randn(m,n),"econ");
 Q = Q*diag(sign(diag(R)));
 end
--- utils/random_ls_problem.m
+++ utils/random_ls_problem.m
@@ -1,9 +1,9 @@
 function [A,b,x,r,S,V] = random_ls_problem(m,n,cond_A,res_size)
-U = haarorth(m);
+U = haarorth(m,n+1);
 V = haarorth(n);
 S = diag(logspace(-log10(cond_A),0,n));
 A = U(:,1:n)*S*V';
 x = orth(randn(n,1));
-r = orth(U(:,(n+1):end)*randn(m-n,1)) * res_size;
+r = U(:,n+1) * res_size;
 b = A*x + r;
 end
