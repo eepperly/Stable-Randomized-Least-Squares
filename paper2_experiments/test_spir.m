a6b029cc8c01666ca275d3e45480ae28e53bbacd
Ethan Epperly <eepperly@caltech.edu>
2024-06-07 17:39:41 -0700

    Add SPIR test

---
+++ paper2_experiments/test_spir.m
@@ -0,0 +1,55 @@
+m = 4000;
+n = 50;
+d = 12*n;
+[A,b,x,r] = random_ls_problem(4000,50,1e12,1e-6);
+trials = 100;
+
+real_run = true;
+if real_run
+    summary = @(y) [norm(y-x)/norm(x);norm(b-A*y-r)/norm(b);backward_error_ls(A,b,y)/norm(A,'fro')];
+else
+    summary = @(y) [norm(y-x)/norm(x);norm(b-A*y-r)/norm(b);kw_estimate(A,b,y,Inf,S,V)/norm(A,'fro')];
+end
+
+[~,spir_lsqr] = spir(A,b,d,[],summary,true);
+[~,spir_cg] = spir(A,b,d,[],summary,true,'sym');
+
+[Q,R] = qr(A,'econ');
+y = R\(Q'*b);
+qr_vals = summary(y);
+
+%% Plot
+
+close all
+for j = 1:3
+    figure(j)
+    semilogy(0:trials, spir_lsqr(:,j), 's-', 'LineWidth', 1, 'Color',...
+        purple, 'MarkerSize', 10, 'MarkerFaceColor', purple); hold on
+    semilogy(0:trials, spir_cg(:,j), 'v--', 'LineWidth', 1, 'Color',...
+        orange, 'MarkerSize', 10, 'MarkerFaceColor', orange); 
+    yline(qr_vals(j),'k:', 'LineWidth', 3)
+    xline(50,'--','LineWidth',3,'Color','#222222')
+    xlabel('Iteration $i$')
+    if j == 1
+        ylabel('Forward error $\|\mbox{\boldmath $x$}-\mbox{\boldmath $\widehat{x}$}_i\|/\|\mbox{\boldmath $x$}\|$')
+        legend('SPIR (LSQR)',...
+            'SPIR (CG)',...
+            'QR')
+	if real_run
+            saveas(gcf,'../figs/spir_forward.fig')
+            saveas(gcf,'../figs/spir_forward.png')
+	end
+    elseif j == 2
+        ylabel('Residual error $\|\mbox{\boldmath $A$}(\mbox{\boldmath $x$}-\mbox{\boldmath $\widehat{x}$}_i)\|/\|\mbox{\boldmath $b$}\|$')
+	if real_run
+            saveas(gcf,'../figs/spir_residual.fig')
+            saveas(gcf,'../figs/spir_residual.png')
+	end
+    elseif j == 3
+        ylabel('Backward error $\mbox{BE}_\infty(\mbox{\boldmath $\widehat{x}$}_i)$')
+	if real_run
+        saveas(gcf,'../figs/spir_backward.fig')
+        saveas(gcf,'../figs/spir_backward.png')
+	end
+    end
+end
