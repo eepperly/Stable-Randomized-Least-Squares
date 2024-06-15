8d291be56eb8a8ee9d233c0bc0ecf8a97eddd5fd
Ethan Epperly <eepperly@caltech.edu>
2024-06-15 12:07:42 -0700

    Unify FOSSILS and SPIR implementations

---
+++ code/metasolver.m
@@ -0,0 +1,163 @@
+function [x,stats,num_iters] = metasolver(A,b,setup,iterate,varargin)
+    Anum = isnumeric(A);
+    if Anum
+        scale = vecnorm(A);
+        A = A ./ scale;
+        m = size(A,1);
+        n = size(A,2);
+        Afun = @(x,op) mul(A,x,op);
+    else
+        scale = 1;
+        m = size(b,1);
+        n = size(A(zeros(size(b,1),0), true),1);
+        Afun = A;
+    end
+
+    if length(varargin) >= 1 && ~isempty(varargin{1})
+        d = varargin{1};
+    else
+        d = 12*n;
+    end
+    
+    if length(varargin) >= 2 && ~isempty(varargin{2})
+        iterations = varargin{2};
+    else
+        iterations = 'adaptive';
+    end
+    
+    if length(varargin) >= 3 && ~isempty(varargin{3})
+        summary = varargin{3};
+    else
+        summary = [];
+    end
+
+    if length(varargin) >= 4 && ~isempty(varargin{4})
+        verbose = varargin{4};
+    else
+        verbose = false;
+    end
+
+    if length(varargin) >= 5 && ~isempty(varargin{5})
+        reproducible = varargin{5};
+    else
+        reproducible = false;
+    end
+    
+    if ~reproducible && exist('sparsesign','file') == 3
+        S = sparsesign(d,m,8);
+    else
+        warning(['Using slower and slightly incorrect backup ' ...
+            'implementation sparse_sign_backup.m. For the better ' ...
+            'implementation, build the mex file `sparsesign.c` ' ...
+            'using the command `mex sparsesign.c`.']);
+        S = sparse_sign_backup(d,m,8);
+    end
+
+    if ~isstring(iterations) && ~ischar(iterations)
+        if length(iterations) == 1 && (floor(iterations) ~= iterations)
+            betol = iterations;
+            iterations = [100,100];
+            adaptive = true;
+        else
+            adaptive = false;
+        end
+    elseif strcmp(iterations, 'adaptive')
+        adaptive = true;
+        iterations = [100,100];
+        betol = eps;
+    else
+        error("Second argument should be a list of integers or 'adaptive'")
+    end
+
+    stats = [];
+
+    if Anum
+        SA = full(S*A);
+    else
+        SA = full(Afun(S',true)');
+    end
+    [U,svals,V] = svd(SA,'econ'); svals = diag(svals);
+    x = V*((U'*(S*b)) ./ svals);
+    r = b - Afun(x,false);
+
+    % Norm and condition number estimation
+    Acond = max(svals) / min(svals);
+    Anorm = max(svals); 
+    Afronorm = norm(svals);
+    bnorm = norm(b);
+
+    if Acond > 1/eps/30
+        reg = 10 * Afronorm * eps;
+        sreg = sqrt(svals.^2 + reg^2);
+    else
+        reg = 0;
+        sreg = svals;
+    end
+
+    betol = Afronorm * betol; % Backward error tolerance
+
+    if ~isempty(summary); stats(end+1,:) = summary(x); end
+    num_iters = 0;
+
+    for loop = 1:length(iterations)
+        c = (V'*(Afun(r,true) - reg^2*x))./sreg;
+        dy = c;
+        solverdata = setup(c,dy,@(xx) matvec(xx,V,sreg,Afun,reg),d);
+
+        if verbose; fprintf('Beginning loop %d\n', loop); end
+        for i = 1:iterations(loop)
+            num_iters = num_iters + 1;
+            [dy,update,solverdata] = iterate(solverdata);
+            if ~isempty(summary)
+                stats(end+1,:) = summary(x + V*(dy./sreg)); %#ok<AGROW>
+            end
+            if verbose
+                xhat = x + V*(dy./sreg);
+                rhat = b - Afun(xhat,false);
+                be = posterior_estimate(Afun,xhat,rhat,V,svals,Afronorm,bnorm);
+                fprintf('Iteration %d\t%e\t%e\n', i, norm(update), be);
+            end
+            if adaptive && loop == 1 && norm(update) ...
+                    <= (Anorm*norm(x) + 0.04*Acond*norm(r))*eps
+                break
+            elseif adaptive && loop == 2 && mod(i,5) == 0
+                xhat = x + V*(dy./sreg);
+                rhat = b - Afun(xhat,false);
+                be = posterior_estimate(Afun,xhat,rhat,V,svals,Afronorm,bnorm);
+                if be <= betol; break; end
+            end
+        end
+        x = x + V*(dy./sreg);
+        r = b - Afun(x,false);
+        if adaptive
+            be = posterior_estimate(Afun,x,r,V,svals,Afronorm,bnorm);
+            if be <= betol; break; end
+            if loop == 2
+                warning('Exiting without backward stability!')
+            end
+        end
+    end
+
+    x = x ./ scale.';
+end
+
+function be = posterior_estimate(Afun,x,r,V,svals,Afronorm,bnorm)
+    theta = Afronorm / bnorm;
+    xnorm = norm(x);
+    be = norm((V'*Afun(r,true)) ./ (svals.^2 ...
+        + theta^2*norm(r)^2/(1+theta^2*xnorm^2)).^0.5) ...
+        * (theta / sqrt(1+theta^2*xnorm^2));
+end
+
+function b = mul(A, x, op)
+    if op
+        b = A'*x;
+    else
+        b = A*x;
+    end
+end
+
+function Ady = matvec(dy,V,sreg,Afun,reg)
+    z = V*(dy./sreg);
+    Ady = (V'*(Afun(Afun(z,false),true) + reg^2 * z)) ./ sreg;
+end
--- code/spir.m
+++ code/spir.m
@@ -1,108 +1,44 @@
-function [x,stats] = spir(A,b,varargin)
-%SPIR Solve A*x = b in the least-squares sense by sketch-and-precondition
-%with iterative refinement
+function [x,stats,num_iters] = spir(A,b,varargin)
+%SPIR Solve A*x = b in the least-squares sense using SPIR
+%(sketch-and-precondition with iterative refinement)
 %   Optional parameters (use [] for default value):
-%   - d: sketching dimension (default 2*n).
-%   - q: number of steps.
+%   - d: sketching dimension (default 12*size(A,2))
+%   - iterations: can be set in three ways
+%        * if iterations is a vector of integers, iterations(i) contains
+%          the number of steps to be taken on the i-th refinement step.
+%        * if iterations is a non-integer, iterations sets the tolerance
+%          for the backward error and the number of steps is determined
+%          adaptively
+%        * if iterations is 'adaptive', the number of steps is determined
+%          adaptively with the default tolerance (default 'adaptive')
 %   - summary: a function of the current iterate to be recorded at each
 %     iteration. All summary values will be rows of stats, the second
 %     output of this function.
-%   - verbose: if true, print at each iteration.
-%   - opts: specify the solver ('cgne' or 'lsqr') and whether to use a
-%     'warm' start (initial iterate given by sketch-and-solve) or a 'cold'
-%     start (initial iterate zero). Defaults to 'lsqrwarm'
-%   - reproducible: if true, use a slow, but reproducible implementation
-%     of sparse sign embeddings. Defaults to false
+%   - verbose: if true, print at each iteration. (default false)
+%   - reproducible: if true, use slower, reproducible implementation of
+%     sparse sign embeddings (default false)
 
-    m = size(A,1);
-    n = size(A,2);
+    [x,stats,num_iters] = metasolver(A,b,@spir_setup,...
+        @spir_iterate,varargin{:});
+end
 
-    if length(varargin) >= 1 && ~isempty(varargin{1})
-        d = varargin{1};
-    else
-        d = 2*n;
-    end
-    
-    if length(varargin) >= 2 && ~isempty(varargin{2})
-        qs = varargin{2};
-    else
-        qs = [50,50];
-    end
-    
-    if length(varargin) >= 3 && ~isempty(varargin{3})
-        summary = varargin{3};
-    else
-        summary = [];
-        mysum = [];
-    end
+function data = spir_setup(c,dy,matvec,d) %#ok<INUSD>
+    data.r = c - matvec(dy);
+    data.rsq = data.r' * data.r;
+    data.p = data.r;
+    data.dy = dy;
+    data.matvec = matvec;
+end
 
-    if length(varargin) >= 4 && ~isempty(varargin{4})
-        verbose = varargin{4};
-    else
-        verbose = false;
-    end
-
-    if length(varargin) >= 5 && ~isempty(varargin{5})
-        opts = varargin{5};
-    else
-        opts = '';
-    end
-
-    if length(varargin) >= 6 && ~isempty(varargin{6})
-        reproducible = varargin{6};
-    else
-        reproducible = false;
-    end
-    
-    if ~reproducible && exist('sparsesign','file') == 3
-        S = sparsesign(d,m,8);
-    else
-        warning(['Using slow implementation sparsesign_slow.m. ' ...
-            'For the better ' ...
-            'implementation, build the mex file `sparsesign.c` ' ...
-            'using the command `mex sparsesign.c`.']);
-        S = sparsesign_slow(d,m,8);
-    end
-
-    [Q,R] = qr(full(S*A),'econ');
-
-    if contains(opts, 'cold')
-        x = zeros(n,1);
-    else
-        x = R \ (Q' * (S*b));
-    end
-
-    for q_idx = 1:length(qs)
-        q = qs(q_idx);
-        if contains(opts, 'cgne')
-            if ~isempty(summary)
-                mysym = @(y) summary(x+y);
-            end
-            [dx,~,newstats] = mycg(@(y) A'*(A*y),@(y) R\(R'\y),...
-                A'*(b-A*x),0,q,mysym,zeros(size(x)),verbose);
-        elseif contains(opts, 'sym')
-            if ~isempty(summary)
-                mysum = @(y) summary(x+R\y);
-            end
-            [dy,~,newstats] = mycg(@(y) R'\(A'*(A*(R\y))),...
-                @(y) y,R'\(A'*(b-A*x)),0,q,mysum,zeros(size(x)),verbose);
-            dx = R\dy;
-        else
-            if ~isempty(summary)
-                mysum = @(y) summary(x+R\y);
-            end
-            [dy,~,newstats] = mylsqr(@(y) A*(R\y),@(y) R'\(A'*y),b-A*x,...
-                0,q,mysum,zeros(size(x)),verbose);
-            dx = R\dy;
-        end
-        x = x + dx;
-        if ~isempty(summary)
-            if q_idx == 1
-                stats = newstats;
-            else
-                stats = [stats;newstats(2:end,:)]; %#ok<AGROW>
-            end
-        end
-    end
-
+function [dy,update,data] = spir_iterate(data)
+    Ap = data.matvec(data.p);
+    alpha = data.rsq / (data.p' * Ap);
+    update = alpha * data.p;
+    data.dy = data.dy + update;
+    dy = data.dy;
+    data.r = data.r - alpha * Ap;
+    new_rsq = data.r'*data.r;
+    beta = new_rsq / data.rsq;
+    data.rsq = new_rsq;
+    data.p = data.r + beta*data.p;
 end
